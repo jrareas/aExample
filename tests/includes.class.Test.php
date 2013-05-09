74aad50128f1cca2f12da25874196962d2b45a82
Jose Areas <jrareas@gmail.com>
2013-05-09 16:42:40 -0400

    adding some unit tests

--- tests/includes.class.Test.php
+++ tests/includes.class.Test.php
@@ -1,11 +1,15 @@
 <?php
-	require_once '../core.class/core.includes.class.php';
-	define('BASE_DIR', dirname( dirname(__FILE__) ));
+	
+	defined('BASE_DIR') or define('BASE_DIR', dirname( dirname(__FILE__) ));
+	require_once BASE_DIR . DIRECTORY_SEPARATOR . 'core.class/core.includes.class.php';
 	class includesTest extends PHPUnit_Framework_TestCase{
 		public function setUp(){}
 		public function tearDown(){}
-		public function testgetInstance(){
+		public function testgetInstanceSelfSmarty(){
 			$this->assertNotNull(CoreIncludes::getInstance('Selfsmarty'));
+		}
+		public function testgetInstanceUsers(){
 			$this->assertNotNull(CoreIncludes::getInstance('Users'));
 		}
+		
 	}
--- tests/users.class.Test.php
+++ tests/users.class.Test.php
@@ -1,9 +1,16 @@
 <?php
-	require_once('../class/users.class.php');
+	defined('BASE_DIR') or define('BASE_DIR', dirname( dirname(__FILE__) ));
+	require_once BASE_DIR . DIRECTORY_SEPARATOR . 'core.class/core.includes.class.php';
 	class UsersTest extends PHPUnit_Framework_TestCase{
-		public function setUp();
-		public function tearDown();
+		protected $user;
+		public function setUp(){
+			$this->user = CoreIncludes::getInstance('Users');
+		}
+		public function tearDown(){
+			unset($this->user);
+		}
 		public function testgetUserByUserId(){
-			
+			$user_arr = $this->user->getUserByUserId('admin');
+			$this->assertArrayHasKey('id',$user_arr) ;
 		}
 	}
